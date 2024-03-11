f034777da55ffccb59aabbdeee04312e9e5fd864
Mohit <2021csb1109@iitrpr.ac.in>
2024-03-12 03:22:03 +0530

    home page improved

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,26 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Home.css";
 import "./Autofill.css";
 
 function Home() {
   let { home_data } = useParams();
-  const [searchTerm, setSearchTerm] = useState("");
-  const [sortColumn, setSortColumn] = useState("id");
-  const [sortDirection, setSortDirection] = useState("asc");
   const [data, setData] = useState([]);
   const [email, setEmail] = useState("");
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSortChange = (column) => {
-    setSortColumn(column);
-    setSortDirection((prevDirection) => (prevDirection === "asc" ? "desc" : "asc"));
-  };
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getEmail = localStorage.getItem("email");
@@ -34,39 +21,14 @@ function Home() {
           headers: { "Content-Type": "application/json" },
         });
 
+        const data2 = await res.json();
+        const parsedData = data2.result.map((item) => {
+          item[4] = JSON.parse(item[4]);
+          return item;
+        });
+        parsedData.sort((a, b) => new Date(b[4].user.date) - new Date(a[4].user.date));
 
-
-
-
-
-
-
-
-        
-        let data2 = await res.json();
-      
-const jsonDataString = JSON.stringify(data2.result);
-
-
-const parsedData = JSON.parse(jsonDataString);
-
-
-
-for(let i=0;i<parsedData.length;i++){
-  
-  parsedData[i][4]=JSON.parse(parsedData[i][4])
-}
-        
-        console.log(parsedData)
-     
-        
         setData(parsedData);
-        
-
-
-
-
-
       } catch (error) {
         console.error("Error fetching application data:", error);
       }
@@ -75,159 +37,106 @@ for(let i=0;i<parsedData.length;i++){
     getApplicationId();
   }, []);
 
-  let navigate = useNavigate();
-
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("isLoggedIn");
     navigate("/");
   };
 
-  const filteredData = data.filter((row) =>
-    Object.values(row).some((value) =>
-      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  );
-
-  const sortedData = filteredData.sort((a, b) => {
-    const isAsc = sortDirection === "asc";
-    if (a[sortColumn] < b[sortColumn]) {
-      return isAsc ? -1 : 1;
-    } else if (a[sortColumn] > b[sortColumn]) {
-      return isAsc ? 1 : -1;
-    } else {
-      return 0;
-    }
-  });
-
   return (
-    <div style={{ display: "flex" }}>
-      {/* Sidebar */}
-      <div id="sidebar1" className="d-flex flex-column flex-shrink-0 p-3 text-white">
-        <h2 className="text_center">Menu</h2>
+    <div className="home-container">
+      <div className="sidebar">
+        <h2 className="text-center">Menu</h2>
         <br />
         <ul className="nav nav-pills flex-column mb-auto">
-          <Link to="/Home" style={{ textDecoration: "none" }}>
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white active">
-                <i className="fa fa-home"></i>
-                <span className="ms-2 font_size_18">Home </span>
-              </a>
-            </li>
-          </Link>
-          <Link to="/Autofill" style={{ textDecoration: "none" }}>
-            <li>
-              <a href="#" className="nav-link text-white">
-                <i className="fa fa-first-order"></i>
-                <span className="ms-2 font_size_18">Auto Fill</span>
-              </a>
-            </li>
-          </Link>
-          <Link to="/Home/Home_verified_applications" style={{ textDecoration: "none" }}>
-            <li>
-              <a className="nav-link text-white" href="#">
-                <i className="fa fa-first-order"></i>
-                <span className="ms-2 font_size_18">Approved applications</span>
-              </a>
-            </li>
-          </Link>
+          <li>
+            <Link to="/Home" className="nav-link active">
+              <i className="fa fa-home"></i>
+              <span className="ms-2 font-size-18">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/Autofill" className="nav-link">
+              <i className="fa fa-first-order"></i>
+              <span className="ms-2 font-size-18">Auto Fill</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/Home/Home_verified_applications" className="nav-link">
+              <i className="fa fa-first-order"></i>
+              <span className="approved-applications ms-3 font-size-20">Approved applications</span>
+            </Link>
+          </li>
           <li onClick={handleLogout}>
-            <a href="/" className="nav-link text-white">
+            <Link to="/" className="nav-link">
               <i className="fa fa-bookmark"></i>
-              <span className="ms-2 font_size_18">Logout</span>
-            </a>
+              <span className="ms-2 font-size-18">Logout</span>
+            </Link>
           </li>
         </ul>
       </div>
 
-      {/* Main Content */}
-      <div style={{ marginBottom: "50px", width: "100%" }}>
-        {/* Top Navbar */}
-        <div id="top_navbar">
-          <div id="btns">
-            <Link to="/Home/Instructions" style={{ textDecoration: "none" }}>
-              <div id="inst_button1"> Instructions</div>
+      <div className="main-content">
+        <div className="top-navbar">
+          <div className="btns">
+            <Link to="/Home/Instructions" className="btn">
+              <div className="inst-button">Instructions</div>
             </Link>
-            <Link to="/Page1" style={{ textDecoration: "none" }}>
-              <div id="apply_button1"> Apply for Reimbursement</div>
+            <Link to="/Page1" className="btn">
+              <div className="apply-button">Apply for Reimbursement</div>
             </Link>
           </div>
           <div>
-            <div id="name1">Welcome</div>
-            <div id="email1">{email}</div>
+            <div className="name">Welcome</div>
+            <div className="email">{email}</div>
           </div>
         </div>
 
-        {/* Last Heading */}
-        <div id="last_heading" style={{ marginLeft: "30px" }}>
-          <h4>Home </h4>
-          <h6>(applications which are yet to be approved by all authority people will appear here)</h6>
+        <div className="last-heading">
+          <h4>Home</h4>
+          {/* <h6>(applications which are yet to be approved by all authority people will appear here)</h6> */}
         </div>
 
-        {/* Application List */}
-        <div className="application_list">
-          <div style={{ margin: "20px" }}>
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={handleSearchChange}
-              className="search-bar"
-            />
-          </div>
-
+        <div className="application-list">
           <table className="table">
             <thead>
               <tr>
-                <th scope="col">
-                  <button className="thbtn" onClick={() => handleSortChange("id")}>
-                    ID
-                    <i className="fa-solid fa-sort" style={{ marginLeft: "4px" }}></i>
-                  </button>
-                </th>
-                <th scope="col">
-                  <button className="thbtn" onClick={() => handleSortChange("amount")}>
-                    Amount Claimed
-                    <i className="fa-solid fa-sort" style={{ marginLeft: "4px" }}></i>
-                  </button>
-                </th>
-                <th scope="col">
-                  <button className="thbtn" onClick={() => handleSortChange("date")}>
-                    Date of submission
-                    <i className="fa-solid fa-sort" style={{ marginLeft: "4px" }}></i>
-                  </button>
-                </th>
+                <th>ID</th>
+                <th>Amount Claimed</th>
+                <th>Date of submission</th>
                 <th>Pharmacist Status</th>
                 <th>Medical Officer Status</th>
                 <th>Account Section Status</th>
                 <th>Registrar Status</th>
+                <th>Action</th> {/* Added Action column */}
               </tr>
             </thead>
             <tbody>
-              {data.map((row) => (
-                <tr
-                  key={row.id}
-                  className="application_id1"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    home_data === "Home"
-                      ? navigate("/ShowApplication/" + row[0])
-                      : navigate("/Home/ShowApplication/" + row[0]);
-                  }}
-                >
-                  {row[0]&& < td>{row[0]}</td>}
-                  {row[4].user.amountClaimed && <td>{row[4].user.amountClaimed}</td>}
-                  {row[4].user.date && <td>{row[4].user.date}</td>}
-                  {row[6]&& <td>{row[6]}</td>}
-                  {row[8]&& <td>{row[8]}</td>}
-                  {row[12]&& <td>{row[12]}</td>}
-                  {row[16]&& <td>{row[16]}</td>}
+              {data.map((row, index) => (
+                <tr key={index}>
+                  <td>{row[0]}</td>
+                  <td>{row[4].user.amountClaimed}</td>
+                  <td>{row[4].user.date}</td>
+                  <td>{row[6]}</td>
+                  <td>{row[8]}</td>
+                  <td>{row[12]}</td>
+                  <td>{row[16]}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        home_data === "Home"
+                          ? navigate("/ShowApplication/" + row[0])
+                          : navigate("/Home/ShowApplication/" + row[0]);
+                      }}
+                      className="btn btn-primary"
+                    >
+                      View Application
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <br />
-          <br />
         </div>
       </div>
     </div>
